b9bedef6e69ececd9b4e9bfffd8db362f9adb071
Madhavkumar-04 <madhavkumarchas@gmail.com>
2025-06-07 19:14:20 +0530

    nltk data

---
+++ Answer-Evaluation-System-main/download_nltk_data.py
@@ -0,0 +1,7 @@
+# download_nltk_data.py
+import nltk
+nltk.download('vader_lexicon')
+nltk.download('wordnet')
+nltk.download('punkt')
+nltk.download('punkt_tab')  # Add this
+nltk.download('stopwords')
